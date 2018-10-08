a5e6328d7db646efd26001c915f873c6a9974151
Joe Evert <joeevert@gmail.com>
2018-10-07 23:08:14 -0500

    updates to functions

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ let employeesArray = [];
 let months = 12;
 
 class Employee {
-    constructor(firstName, lastName, idNum, jobTitle, annualSalary ){
+    constructor(firstName, lastName, idNum, jobTitle, annualSalary ) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.idNum = idNum;
@@ -12,88 +12,120 @@ class Employee {
     } // end constructor
 } // end Employee class
 
-$(document).ready(readyNow);
+$(readyNow);
 
 function readyNow() {
+    console.log('jq');
+        
     $('#addEmployee').on('click', addNewEmployee);
-    $('#employeeList').on('click', '.removeEmployee', removeEmployee);
+    $('#addEmployee').on('click', appendNewEmployee);
+    $('#addEmployee').on('click', appendMonthlyTotal);
+    $('.employeeList').on('click', '.removeEmployee', removeEmployee);
 } // end readyNow
 
 // add new employee
 function addNewEmployee() {
     event.preventDefault();
-    console.log('add employee');
+    console.log('in addNewEmployee');
 
     let firstNameIn = $('#firstName').val();
     let lastNameIn = $('#lastName').val();
     let idNumIn = $('#id').val();
     let jobTitleIn = $('#jobTitle').val();
     let annualSalaryIn = $('#annualSalary').val();
-    let newEmployee = new Employee(firstNameIn, lastNameIn, idNumIn, jobTitleIn, annualSalaryIn);
+    let newEmployee = new Employee(firstNameIn, lastNameIn, idNumIn, jobTitleIn, parseInt(annualSalaryIn));
 
+    // add employee to employeesArray
     employeesArray.push(newEmployee);
     console.log(employeesArray);
-    appendNewEmployee();
+    
+    // clear inputs
+    clearInputs();
 } // end addNewEmployee
 
 // append new employee to table
 function appendNewEmployee() {
-    console.log('append New Employees');
-    let totalSalaries = 0;
-    let employeeList = $('#employeeList');
+    console.log('in appendNewEmployee');
+    let employeeList = $('.employeeList');
     employeeList.empty();
 
     // loop employee properties
-    for(employee of employeesArray) {
-        let element = `<tr> <td>${employee.firstName}</td> <td>${employee.lastName}</td> <td>${employee.idNum}</td> 
-        <td>${employee.jobTitle}</td> <td>${employee.annualSalary}</td> 
-        <td><button class='removeEmployee'>Remove</button></td> </tr>`;
-        employeeList.append(element);
-
-        // calculate monthly total of annual salaries
-        totalSalaries += parseInt(employee.annualSalary) / months;
+    for (let employee of employeesArray) {
+        let firstName = `<td>${employee.firstName}</td>`;
+        let lastName = `<td>${employee.lastName}</td>`;
+        let idNum = `<td>${employee.idNum}</td>`;
+        let jobTitle = `<td>${employee.jobTitle}</td>`;
+        let annualSalary = `<td>${employee.annualSalary}</td>`;
+        let removeEmp = '<td><button class="removeEmployee">Remove</button></td>';
+        let element = '<tr>' + firstName + lastName + idNum + jobTitle + annualSalary + removeEmp + '</tr>';
+        $('table tbody').append(element);
     } // end for of
-    appendMonthlyTotal(totalSalaries);
-    // clear inputs
+} // end appendNewEmployee
+
+// append monthly cost
+function appendMonthlyTotal() {
+    console.log('in appendMonthlyTotal');
+    let totalMonthly = $('#totalMonthly');
+    totalMonthly.empty();
+    let totalSalaries = 0;
+    for (let salary of employeesArray) {   
+        totalSalaries += salary.annualSalary / months;
+        console.log(totalSalaries);
+        if (totalSalaries > totalMonthlySalaries) {
+            totalMonthly.parent().toggleClass('exceed');
+            console.log('exceeded budget');
+        } // end exceeds $20,000
+    }
+    totalMonthly.append(`Total Monthly: $${totalSalaries.toFixed(2)}`);
+    return totalSalaries;
+} // end monthly appendMonthlyTotal
+
+// remove employee
+function removeEmployee() {
+    console.log('in removeEmployee');
+    let selectedItem = $(this).closest('tr').find('td').text();
+    console.log(selectedItem);
+    for (let i = 0; i < employeesArray.length; i++) {
+        if (selectedItem.includes(employeesArray[i].idNum)) {
+            employeesArray.splice(i, 1);
+            $(this).closest('tr').find('td').remove();
+            // logic for updating total monthly...
+            appendMonthlyTotal();
+            return true;
+        } // end if 'td' has idNum remove 'tr'???
+    } // end for loop
+} // end removeEmployee
+
+// clear inputs
+function clearInputs() {
     $('#firstName').val('');
     $('#lastName').val('');
     $('#id').val('');
     $('#jobTitle').val('');
     $('#annualSalary').val('');
-} // end appendNewEmployee
+} // end clearInputs
 
-// append monthly cost
-function appendMonthlyTotal(allSalaries) {
-    console.log('total monthly cost');
-    let totalMonthly = $('#totalMonthly');
-        totalMonthly.empty();
-        totalMonthly.append(`<h2>Total Monthly:$${allSalaries}</h2>`)
-    if( allSalaries > totalMonthlySalaries ) {
-        totalMonthly.addClass('exceed');
-    } // end exceeds $20,000
-} // end monthly appendMonthlyTotal
 
-// remove employee
-function removeEmployee() {
-    console.log('remove employee');
-    //let selectedItem = $(this).parent().text();
-    let selectedItem = $(this).closest('tr').find('td').text();
-    console.log(selectedItem);
-    for (let i = 0; i < employeesArray.length; i++) {
-        if (selectedItem.includes(employeesArray[i].lastName)){
-            console.log('remove me');
-            employeesArray.splice(i, 1);
-            $(this).closest('tr').remove();
-        } // end if 'td' has lastName remove 'tr'
-    } // end for loop
-} // end removeEmployee
 
-// some issues with removing still...sometimes it won't remove the 'tr'.
 
-// base mode almost complete...change CSS to match wireframe
 
-// update monthly total on dom for removal
 
-// add a rollover to highlight 'tr'
+// check input fields are complete
+/*function checkInputs(){
+    console.log('in checkInputs');
+    
+    if ( $('#firstName').val() == '' || $('#lastName').val() == '' || $('#id').val() == '' || $('#jobTitle').val() == '' || $('#annualSalary').val() == '' ){
+        console.log( 'Not all input fields completed.' );
+        alert('no')
+      } // end input field checks
+}*/
 
-// an alert box to confirm employee removal?
+// required feature not working on submit...need to get checkInputs field working.
+
+// an alert box to confirm employee removal???
+
+// need to resolve NaN?
+
+// toggle not working correctly...tried an if statement.
+
+// delete removes bottom border of table.
